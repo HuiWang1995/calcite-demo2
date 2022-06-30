6ba3b370f0b2e7e0dba5e0b013b30191a3a3fdbd
mashu <189192663@qq.com>
2022-07-01 01:10:37 +0800

    添加对基础框架调用的注释

--- src/test/java/com/gerardnico/calcite/CalciteFrameworksTest.java
+++ src/test/java/com/gerardnico/calcite/CalciteFrameworksTest.java
@@ -35,43 +35,52 @@ public class CalciteFrameworksTest {
     @Test
     public void parseValidateAndLogicalPlanTest() throws SqlParseException, RelConversionException, ValidationException, SQLException {
 
-        // Build the schema
+        // Build the schema -- calcite构造一个根schema
         SchemaPlus rootSchema = Frameworks.createRootSchema(true);
+        // 创建暴露 属性、方法的 schema， 并初始化了数据emps 、 depts
         ReflectiveSchema schema = new ReflectiveSchema(new HrSchemaMin());
+        // 走超类的add  org.apache.calcite.jdbc.CalciteSchema.SchemaPlusImpl.add()
+        // 通过debug可以看到，最后返回的是一个SchemaPlusImpl,且schema属于rootSchema的sub（子）
         SchemaPlus hr = rootSchema.add("HR", schema);
 
-        // Get a non-sensitive parser
+        // Get a non-sensitive parser -- 设置大小写不敏感
         SqlParser.Config insensitiveParser = SqlParser.configBuilder()
                 .setCaseSensitive(false)
                 .build();
 
         // Build a global configuration object
         FrameworkConfig config = Frameworks.newConfigBuilder()
+                // 设置sql 粘贴的配置
                 .parserConfig(insensitiveParser)
+                // 设置默认schema
                 .defaultSchema(hr)
                 .build();
 
-        // Get the planner tool
+        // Get the planner tool -- 获取查询计划
         Planner planner = Frameworks.getPlanner(config);
 
-        // Parse the sql to a tree
+        // Parse the sql to a tree -- 通过查询计划粘贴SQL，转化为sqlNode
         SqlNode sqlNode = planner.parse("select depts.name, count(emps.empid) from emps inner join depts on emps.deptno = depts.deptno group by depts.deptno, depts.name order by depts.name");
 
         // Print it
         System.out.println(sqlNode.toSqlString(OracleSqlDialect.DEFAULT));
 
         // Validate the tree
+        // 查询计划校验sqlNode
         SqlNode sqlNodeValidated = planner.validate(sqlNode);
 
         // Convert the sql tree to a relation expression
+        // 将校验结束的sqlNode转为关系表达式
         RelRoot relRoot = planner.rel(sqlNodeValidated);
 
         // Explain, print the relational expression
+        // 投影，较少不需要查询的属性
         RelNode relNode = relRoot.project();
         final RelWriter relWriter = new RelWriterImpl(new PrintWriter(System.out), SqlExplainLevel.ALL_ATTRIBUTES, false);
+        // org.apache.calcite.rel.AbstractRelNode.explain 实际解释
         relNode.explain(relWriter);
 
-        // Run it
+        // Run it -- 预解析
         PreparedStatement run = RelRunners.run(relNode);
         ResultSet resultSet = run.executeQuery();
 
